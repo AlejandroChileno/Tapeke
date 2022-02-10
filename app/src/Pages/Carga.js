ff1699530f2b2aa13e7eda4aa0ad1aa895078b3d
Licethd <liceth.dfe@gmail.com>
2022-02-10 18:39:31 -0400

    all

--- app/src/Pages/Carga.js
+++ app/src/Pages/Carga.js
@@ -28,20 +28,9 @@ class Carga extends Component {
         this.hilo()
         return (
             <SPage hidden disableScroll center>
-                <SHr height={32} />
+                <SHr height={52} />
                 <SView col={"xs-9 sm-7 md-5 lg-4 xl-3"} height={200}>
                     <SIcon name={"Logo"} />
-                </SView>
-                <SHr height={32} />
-                <SView col={"xs-10 sm-9 md-7 lg-5 xl-4"} flex >
-                    <SView style={{
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        right: "-35%",
-                    }}>
-                        <SIcon name={`Enfermera4`} fill={"#f0f"} />
-                    </SView>
                 </SView>
                 <SHr height={32} />
                 {/* <SView col={"xs-8 sm-6 md-4 lg-3 xl-2"} height={200}>
