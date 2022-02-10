8214d80f3baab68e19c99ef071bc264eb1aa7a1b
rickypazd <35882906+rickypazd@users.noreply.github.com>
2022-02-10 17:26:32 -0400

    commit

--- app/src/Assets/index.ts
+++ app/src/Assets/index.ts
@@ -18,55 +18,9 @@
 import { SAssets } from 'servisofts-component'
 
 
-import Enfermera1, { ReactComponent as Enfermera1W } from './svg/enfermera/1.svg';
-import Enfermera2, { ReactComponent as Enfermera2W } from './svg/enfermera/2.svg';
-import Enfermera3, { ReactComponent as Enfermera3W } from './svg/enfermera/3.svg';
-import Enfermera4, { ReactComponent as Enfermera4W } from './svg/enfermera/4.svg';
-import Enfermera5, { ReactComponent as Enfermera5W } from './svg/enfermera/5.svg';
-import Enfermera6, { ReactComponent as Enfermera6W } from './svg/enfermera/6.svg';
-import Enfermera7, { ReactComponent as Enfermera7W } from './svg/enfermera/7.svg';
-import Enfermera8, { ReactComponent as Enfermera8W } from './svg/enfermera/8.svg';
-import Enfermera9, { ReactComponent as Enfermera9W } from './svg/enfermera/9.svg';
-import Enfermera10, { ReactComponent as Enfermera10W } from './svg/enfermera/10.svg';
-import Enfermera11, { ReactComponent as Enfermera11W } from './svg/enfermera/11.svg';
-import Enfermera12, { ReactComponent as Enfermera12W } from './svg/enfermera/12.svg';
-import Enfermera13, { ReactComponent as Enfermera13W } from './svg/enfermera/13.svg';
-import Enfermera14, { ReactComponent as Enfermera14W } from './svg/enfermera/14.svg';
-import Enfermera15, { ReactComponent as Enfermera15W } from './svg/enfermera/15.svg';
-import Enfermera16, { ReactComponent as Enfermera16W } from './svg/enfermera/16.svg';
-import Enfermera17, { ReactComponent as Enfermera17W } from './svg/enfermera/17.svg';
-import Enfermera18, { ReactComponent as Enfermera18W } from './svg/enfermera/18.svg';
-import Enfermera19, { ReactComponent as Enfermera19W } from './svg/enfermera/19.svg';
-import Enfermera20, { ReactComponent as Enfermera20W } from './svg/enfermera/20.svg';
-import Enfermera21, { ReactComponent as Enfermera21W } from './svg/enfermera/21.svg';
-import Enfermera22, { ReactComponent as Enfermera22W } from './svg/enfermera/22.svg';
-import Enfermera23, { ReactComponent as Enfermera23W } from './svg/enfermera/23.svg';
-import Enfermera24, { ReactComponent as Enfermera24W } from './svg/enfermera/24.svg';
-import Enfermera25, { ReactComponent as Enfermera25W } from './svg/enfermera/25.svg';
-import Enfermera26, { ReactComponent as Enfermera26W } from './svg/enfermera/26.svg';
-import Enfermera27, { ReactComponent as Enfermera27W } from './svg/enfermera/27.svg';
 
-import SFarmacia, { ReactComponent as SFarmaciaW } from './svg/servicios/sfarmacia.svg';
-import SOptica, { ReactComponent as SOpticaW } from './svg/servicios/soptica.svg';
-import SLaboratorio, { ReactComponent as SLaboratorioW } from './svg/servicios/slaboratorio.svg';
-import SEnfermeria, { ReactComponent as SEnfermeriaW } from './svg/servicios/senfermeria.svg';
-import SImagenologia, { ReactComponent as SImagenologiaW } from './svg/servicios/simagenologia.svg';
-import SControl, { ReactComponent as SControlW } from './svg/servicios/scontrol.svg';
-import SVacunacion, { ReactComponent as SVacunacionW } from './svg/servicios/svacunacion.svg';
-import SCirugias, { ReactComponent as SCirugiasW } from './svg/servicios/scirugias.svg';
-import SPlanificacion, { ReactComponent as SPlanificacionW } from './svg/servicios/splanificacion.svg';
-import SBiopsias, { ReactComponent as SBiopsiasW } from './svg/servicios/sbiopsias.svg';
-import SServicios, { ReactComponent as SServiciosW } from './svg/servicios/sservicios.svg';
-import SDfarmacia, { ReactComponent as SDfarmaciaW } from './svg/servicios/Dfarmacia.svg';
-import SDoptica, { ReactComponent as SDopticaW } from './svg/servicios/Doptica.svg';
-import SDlaboratorio, { ReactComponent as SDlaboratorioW } from './svg/servicios/Dlaboratorio.svg';
-import Dpendiente, { ReactComponent as DpendienteW } from './svg/servicios/Dpendiente.svg';
-import Drechazado, { ReactComponent as DrechazadoW } from './svg/servicios/Drechazado.svg';
 
-import Diciembre, { ReactComponent as DiciembreW } from './svg/sucursales/diciembre.svg';
 import ArrowRight, { ReactComponent as ArrowRightW } from './svg/sucursales/arrowRight.svg';
-import Chacos, { ReactComponent as ChacosW } from './svg/sucursales/chacos.svg';
-import Paraiso, { ReactComponent as ParaisoW } from './svg/sucursales/paraiso.svg';
 import Map, { ReactComponent as MapW } from './svg/sucursales/map.svg';
 import Cellphone, { ReactComponent as CellphoneW } from './svg/sucursales/cellphone.svg';
 
@@ -107,7 +61,6 @@ import KNotify, { ReactComponent as KNotifyW } from './svg/notify.svg';
 import Kolping, { ReactComponent as KolpingW } from './svg/kolping.svg';
 import Consultas, { ReactComponent as ConsultasW } from './svg/consultas.svg';
 import Compras, { ReactComponent as ComprasW } from './svg/compras.svg';
-import LogoTuVidaEsMejor, { ReactComponent as LogoTuVidaEsMejorW } from './svg/tuvidaesmejor.svg';
 import LogoWhite, { ReactComponent as LogoWhiteW } from './svg/logoWhite.svg';
 import Flecha1, { ReactComponent as Flecha1W } from './svg/flecha1.svg';
 import Flecha2, { ReactComponent as Flecha2W } from './svg/flecha2.svg';
@@ -149,33 +102,6 @@ import Camera, { ReactComponent as CameraW } from './svg/camera.svg';
 import BackArrow, { ReactComponent as BackArrowW } from './svg/backArrow.svg';
 const Assets: SAssets = {
     svg: {
-        "Enfermera1": { Native: Enfermera1, Web: Enfermera1W },
-        "Enfermera2": { Native: Enfermera2, Web: Enfermera2W },
-        "Enfermera3": { Native: Enfermera3, Web: Enfermera3W },
-        "Enfermera4": { Native: Enfermera4, Web: Enfermera4W },
-        "Enfermera5": { Native: Enfermera5, Web: Enfermera5W },
-        "Enfermera6": { Native: Enfermera6, Web: Enfermera6W },
-        "Enfermera7": { Native: Enfermera7, Web: Enfermera7W },
-        "Enfermera8": { Native: Enfermera8, Web: Enfermera8W },
-        "Enfermera9": { Native: Enfermera9, Web: Enfermera9W },
-        "Enfermera10": { Native: Enfermera10, Web: Enfermera10W },
-        "Enfermera11": { Native: Enfermera11, Web: Enfermera11W },
-        "Enfermera12": { Native: Enfermera12, Web: Enfermera12W },
-        "Enfermera13": { Native: Enfermera13, Web: Enfermera13W },
-        "Enfermera14": { Native: Enfermera14, Web: Enfermera14W },
-        "Enfermera15": { Native: Enfermera15, Web: Enfermera15W },
-        "Enfermera16": { Native: Enfermera16, Web: Enfermera16W },
-        "Enfermera17": { Native: Enfermera17, Web: Enfermera17W },
-        "Enfermera18": { Native: Enfermera18, Web: Enfermera18W },
-        "Enfermera19": { Native: Enfermera19, Web: Enfermera19W },
-        "Enfermera20": { Native: Enfermera20, Web: Enfermera20W },
-        "Enfermera21": { Native: Enfermera21, Web: Enfermera21W },
-        "Enfermera22": { Native: Enfermera22, Web: Enfermera22W },
-        "Enfermera23": { Native: Enfermera23, Web: Enfermera23W },
-        "Enfermera24": { Native: Enfermera24, Web: Enfermera24W },
-        "Enfermera25": { Native: Enfermera25, Web: Enfermera25W },
-        "Enfermera26": { Native: Enfermera26, Web: Enfermera26W },
-        "Enfermera27": { Native: Enfermera27, Web: Enfermera27W },
         "IconFaceb": { Native: IconFaceb, Web: IconFacebW },
         "IconGoogle": { Native: IconGoogle, Web: IconGoogleW },
         "IconChecked": { Native: IconChecked, Web: IconCheckedw },
@@ -216,30 +142,9 @@ const Assets: SAssets = {
         "cotizacion": { Native: cotizacion, Web: cotizacionW },
         "lapiz": { Native: lapiz, Web: lapizW },
 
-        "sfarmacia": { Native: SFarmacia, Web: SFarmaciaW },
-        "soptica": { Native: SOptica, Web: SOpticaW },
-        "slaboratorio": { Native: SLaboratorio, Web: SLaboratorioW },
-        "senfermeria": { Native: SEnfermeria, Web: SEnfermeriaW },
-        "simagenologia": { Native: SImagenologia, Web: SImagenologiaW },
-        "scontrol": { Native: SControl, Web: SControlW },
-        "svacunacion": { Native: SVacunacion, Web: SVacunacionW },
-        "scirugias": { Native: SCirugias, Web: SCirugiasW },
-        "splanificacion": { Native: SPlanificacion, Web: SPlanificacionW },
-        "sbiopsias": { Native: SBiopsias, Web: SBiopsiasW },
-        "sservicios": { Native: SServicios, Web: SServiciosW },
-        "SDfarmacia": { Native: SDfarmacia, Web: SDfarmaciaW },
-        "SDlaboratorio": { Native: SDlaboratorio, Web: SDlaboratorioW },
-        "SDoptica": { Native: SDoptica, Web: SDopticaW },
-        "Dpendiente": { Native: Dpendiente, Web: DpendienteW },
-        "Drechazado": { Native: Drechazado, Web: DrechazadoW },
-
-        "diciembre": { Native: Diciembre, Web: DiciembreW },
         "arrowRight": { Native: ArrowRight, Web: ArrowRightW },
-        "chacos": { Native: Chacos, Web: ChacosW },
-        "paraiso": { Native: Paraiso, Web: ParaisoW },
         "map": { Native: Map, Web: MapW },
         "cellphone": { Native: Cellphone, Web: CellphoneW },
-        "tuvidaesmejor": { Native: LogoTuVidaEsMejor, Web: LogoTuVidaEsMejorW },
         "logowhite": { Native: LogoWhite, Web: LogoWhiteW },
         "flecha1": { Native: Flecha1, Web: Flecha1W },
         "flecha2": { Native: Flecha2, Web: Flecha2W },
