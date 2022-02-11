433d16c7f8b1c8208186ad145c66fbd28bec62b1
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2022-02-10 20:05:38 -0400

    Billetera finalizada
    Billetera finalizada

--- app/src/Pages/Billetera.js
+++ app/src/Pages/Billetera.js
@@ -2,69 +2,78 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { SHr, SNavigation, SPage, SText, SView, STheme, SLoad, SButtom, SIcon, SWebView } from 'servisofts-component';
 import { WebView } from 'react-native';
+import PButtom from '../Components/PButtom';
+
+
 class Kolping extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
     }
-    getDetalleBilletera(fecha, descripcion, tipoIcon, monto) {
-        return <SView col={"xs-12"} center >
-            <SView onPress={() => { SNavigation.navigate("ficha/horarios"); }}
-                col={"xs-12"} row backgroundColor={STheme.color.card} height={52} center style={{ borderRadius: 8 }}>
 
-
-
-                <SView col={"xs-8"} >
-                    <SText font={"Manrope"} color={STheme.color.lightGray} fontSize={10} style={{ paddingLeft: 10 }} >{fecha}</SText>
-                    <SText font={"Manrope"} color={STheme.color.text} fontSize={12} style={{ paddingLeft: 10 }} >{descripcion}</SText>
-                </SView>
-
-                <SView col={"xs-4"} style={{ textAlign: "right" }} row center>
-                    <SIcon name={tipoIcon} width={20} height={15} fill={"#8DBF3B"} />
-                    <SView col={"xs-1"} />
-                    <SText font={"Manrope"} color={STheme.color.text} fontSize={10}>{monto}</SText>
+    getHeaderBilletera(montoTotal, key_usuario) {
+        return <> <SHr height={10} />
+            <SView col={"xs-12"} height={201} center>
+                <SIcon name={'Chip'} width={290} height={181} style={{ position: "absolute" }} />
+                <SView width={250} height={140}>
+                    <SView style={{ top: 20, left: 0, position: "absolute" }}>
+                        <SText fontSize={14} color={'#ffffffbb'} font={"Roboto"} >Monto actual</SText>
+                        <SText fontSize={28} color={'#ffffffbb'} font={"Roboto"} >Bs. {montoTotal}</SText>
+                    </SView>
+                    <SView style={{ bottom: 0, right: 0, position: "absolute" }}>
+                        <SText fontSize={13} color={'#ffffffbb'} font={"Roboto"} >{key_usuario}</SText>
+                    </SView>
                 </SView>
             </SView>
-            <SHr height={15} />
-        </SView>
+        </>
+    }
 
+    getDetalleBilletera(fecha, descripcion, tipoIcon, monto) {
+        return <> <SHr height={10} />
+            <SView col={"xs-12"} center >
+                <SView col={"xs-12"} row backgroundColor={STheme.color.card} height={52} center style={{ borderRadius: 8 }}>
+                    <SView col={"xs-8"} >
+                        <SText font={"Manrope"} color={STheme.color.lightGray} fontSize={10} style={{ paddingLeft: 10 }} >{fecha}</SText>
+                        <SText font={"Manrope"} color={STheme.color.text} fontSize={12} style={{ paddingLeft: 10 }} >{descripcion}</SText>
+                    </SView>
+                    <SView col={"xs-4"} style={{ textAlign: "right" }} row center>
+                        <SIcon name={tipoIcon} width={20} height={15} fill={"#8DBF3B"} />
+                        <SView col={"xs-1"} />
+                        <SText font={"Manrope"} color={STheme.color.text} fontSize={10}>{monto}</SText>
+                    </SView>
+                </SView>
+            </SView>
+        </>
     }
 
     render() {
         return (
-            <SPage title={'Billetera'}>
-                <SView col={"xs-12"} center>
+            <>
+                <SPage title={'Billetera'}>
+                    <SView col={"xs-12"} center>
+                        {this.getHeaderBilletera('120,00', '5294 2436 4780 2468')}
+                        <SHr height={18} color={STheme.color.card} />
 
-                    <SHr height={15} />
+                        <SView col={"xs-8"} row center>
+                            <SHr height={10} />
+                            <PButtom fontSize={20} onPress={() => { alert('alvaro'); }}>CARGAR CRÉDITO</PButtom>
+                            <SHr height={10} />
+                        </SView>
+                        <SHr height={18} color={STheme.color.card} />
 
-                    <SView col={"xs-12"}>
-                        <SView col={"xs-12"} row center>
-                            <SIcon name={'Chip'} width={190} height={181} />
-
+                        <SView col={"xs-11 md-8 lg-6 xl-4"}>
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Compra de producto', 'Egreso', 'Bs. -80.00')}
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Compra de producto', 'Egreso', 'Bs. -80.00')}
+                            {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
+                            <SHr height={50} />
                         </SView>
                     </SView>
-
-                    <SHr height={18} />
-                    <SView col={"xs-12"} backgroundColor={'red'}>
-                        <SButtom primary >CARGAR CRÉDITO</SButtom>
-                    </SView>
-
-                    <SHr height={18} />
-
-
-                    <SView col={"xs-11"}>
-
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Compra de producto', 'Egreso', 'Bs. -80.00')}
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Compra de producto', 'Egreso', 'Bs. -80.00')}
-                        {this.getDetalleBilletera('Enero, 06 - 16:45', 'Recarga de crédito', 'Ingreso', 'Bs. 100.00')}
-
-                    </SView>
-                </SView>
-            </SPage>
+                </SPage>
+            </>
         );
     }
 }
