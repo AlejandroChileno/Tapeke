bd6042518a872b9961946f50efc823b7d876f6bd
Licethd <liceth.dfe@gmail.com>
2022-03-07 20:39:48 -0400

    ok

--- app/src/Services/Tapeke/index.ts
+++ app/src/Services/Tapeke/index.ts
@@ -1,16 +1,19 @@
 import { SPageListProps } from 'servisofts-component'
 import intro from "./Components/intro"
 import restaurante from "./Components/restaurante"
+import pedido from "./Components/pedido"
 
 const ServiceName = "tapeke";
 
 const Pages: SPageListProps = {
     ...intro.Pages,
     ...restaurante.Pages,
+    ...pedido.Pages,
 }
 
 const Reducers = {
     ...restaurante.Reducers,
+    ...pedido.Reducers,
 }
 
 export default {
