018b57688f0d56f80e81644702d952a49225b4a8
Alvaro Siles <alvarosiles1@gmail.com>
2022-03-07 22:30:56 -0400

    master
    master

--- app/src/Services/Tapeke/Components/restaurante/index.ts
+++ app/src/Services/Tapeke/Components/restaurante/index.ts
@@ -4,18 +4,16 @@ const version = "2.0";
 // ---------------------------------------
 import Actions from "./Actions";
 import Reducer from "./Reducer";
-
 import Perfil from "./Pages/Perfil";
-
 import Item from "./Components/Item";
 import Item2 from "./Components/Item2";
 import Filtros from "./Components/Filtros";
-
 import Favoritos from "./Pages/Favoritos";
 import Categoria from "./Pages/Categoria";
 import Explorador from "./Pages/Explorador";
 import lista from "./Pages/lista";
 import registro from "./Pages/registro";
+import ExploradorMapa from "./Pages/ExploradorMapa";
 export default {
     component,
     version,
@@ -29,13 +27,13 @@ export default {
         [component + "/categoria"]: Categoria,
         [component + "/filtros"]: Filtros,
         [component + "/explorador"]: Explorador,
+        [component + "/exploradorMapa"]: ExploradorMapa,
         ["admin/" + component]: lista,
-        ["admin/" + component+"/registro"]: registro,
+        ["admin/" + component + "/registro"]: registro,
 
     },
     Components: {
         Item,
         Item2,
-
     }
 }
