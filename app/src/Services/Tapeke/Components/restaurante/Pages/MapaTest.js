c0592c7256a4e15b87bf33e020e286defe7ae923
Alvaro Siles <alvarosiles1@gmail.com>
2022-03-22 20:46:33 -0400

    Master alvaro
    Master alvaro

--- app/src/Services/Tapeke/Components/restaurante/Pages/MapaTest.js
+++ app/src/Services/Tapeke/Components/restaurante/Pages/MapaTest.js
@@ -37,18 +37,6 @@ class MapaTest extends Component {
 						latitudeDelta: 0.0922,
 						longitudeDelta: 0.0421,
 					}}
-					// onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}
-					// onRegionChangeComplete={(region, gesture) => {
-					// 	if (Platform.OS === 'android') {
-					// 	  if (gesture.isGesture) {
-					// 		onRegionChange(region);
-					// 	  }
-					// 	} else {
-					// 	  onRegionChange(region);
-					// 	}
-
-					// }}
-
 					onRegionChangeComplete={(region) => {
 						console.log(region);
 						this.setState({ region: region })
@@ -63,7 +51,7 @@ class MapaTest extends Component {
 			<SView  style={{ position: 'absolute', }} center   >
 				{/* {onRegionChange(region) } */}
 
-				<SIcon name="Marker" width={20} height={20} fill="red" />
+				<SIcon name="MarcadorMapa" width={47} height={87}  />
 			</SView>
 		</>
 	}
@@ -71,28 +59,16 @@ class MapaTest extends Component {
 
 	render() {
 		return (
-
 			<SPage title={''} hidden disableScroll center>
 				<BarraSuperiorTapeke  >
 					<SText font={"Roboto"} fontSize={25} color={STheme.color.secondary}>Mis Favoritos</SText>
 				</BarraSuperiorTapeke>
-
 				<SScrollView2 disableHorizontal={true}>
-					<SView col={"xs-12"} center height border={'blue'} >
+					<SView col={"xs-12"} center height  >
 						{this.showMapa()}
 					</SView >
-
-					{/* <SView backgroundColor={'red'} width={180} height={50} style={{ position: 'absolute', top: 150,   }}row  center >
-					<SText font={"Roboto"} fontSize={18} color={STheme.color.secondary}>Mis Favoritos</SText>
-					<SText font={"Roboto"} fontSize={18} color={STheme.color.secondary}>Mis Favoritos</SText>
-
-					</SView> */}
-
-					<SHr height={80} />
-				</SScrollView2>
-
+ 				</SScrollView2>
 				<PBarraFooter />
-
 			</ SPage >
 		);
 	}
