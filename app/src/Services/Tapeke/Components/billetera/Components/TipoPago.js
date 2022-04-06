68108af2f2ea0eb3513b4496c239f3dcddc02d8b
Alvaro Siles <alvarosiles1@gmail.com>
2022-04-05 21:44:52 -0400

    cambios
    cambios

--- app/src/Services/Tapeke/Components/billetera/Components/TipoPago.js
+++ app/src/Services/Tapeke/Components/billetera/Components/TipoPago.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { SHr, SIcon, SImage, SNavigation, SText, STheme, SView,SLoad } from 'servisofts-component';
+import { SHr, SIcon, SImage, SNavigation, SText, STheme, SView, SLoad } from 'servisofts-component';
 import SSocket from 'servisofts-socket';
 import restaurante from '../../restaurante';
 
@@ -9,36 +9,223 @@ class TipoPago extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+
+        this.state = {
+
+            pagado: 0,
+
+        };
     }
 
 
-
-    item() {
-
-        var data_restaurante = restaurante.Actions.getByKey(this.props.data.key_restaurante, this.props);
-        if (!data_restaurante) return <SLoad />;
-
+    item(key, imagen, descripcion) {
         return <>
-            <SView col={"xs-11"} height={100} border={'blue'} row>
-                <SText col={"xs-12"} fontSize={12} font={"Roboto"}>Restaurante: {data_restaurante.nombre}</SText>
-                 <SText col={"xs-12"} fontSize={12} font={"Roboto"}>Dia: {this.props.data.dia}</SText>
-                <SText col={"xs-12"} fontSize={12} font={"Roboto"}>Apertura: {this.props.data.hora_inicio}</SText>
-                <SText col={"xs-12"} fontSize={12} font={"Roboto"}>Cierre: {this.props.data.hora_fin}</SText>
-                <SText col={"xs-12"} fontSize={12} font={"Roboto"}>Estado: {this.props.data.estado}</SText>
+            <SHr height={15} />
+            <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 600 }); }}>
+                <SView col={"xs-2"} >
+                    <SView center style={{
+                        width: 40,
+                        height: 40, borderRadius: 8,
+                        borderWidth: 1, borderColor: STheme.color.lightGray
+                    }}>
+                        <SImage src={imagen} style={{
+                            borderRadius: 8,
+                            width: 30
+                        }} />
+                    </SView>
+                </SView>
+                <SView col={"xs-8"} >
+                    <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>{descripcion} </SText>
+                </SView>
+                <SView col={"xs-2"} center>
+                    <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                </SView>
             </SView>
-            <SHr height={10} />
         </>
     }
 
 
+
+
+    getTipoPago() {
+
+
+        return <>
+            <SHr height={15} />
+            <SView col={"xs-11 sm-6 lg-4"} border={'transparent'} row>
+                <SView col={"xs-12"} row style={{ justifyContent: 'flex-start', }}>
+                    <SText font={"Roboto"} style={{ color: STheme.color.primary, fontSize: 18 }} >Tipo de pago</SText>
+                </SView>
+                {/* <SView col={"xs-2"} row style={{ justifyContent: 'flex-end', }}>
+                    <SIcon fill={STheme.color.primary} name={"Add"} width={20} height={20} />
+                </SView> */}
+            </SView>
+            <SHr height={15} />
+            <SView col={"xs-11 sm-6 lg-4"} row center style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 6 }}>
+                <SView col={"xs-11"} row >
+                    <SView col={"xs-12"} row center>
+                        <SHr height={15} />
+
+
+                        {this.item("0001", require('../../../../../Assets/img/efectivo.png'), "tigo")}
+
+                    </SView>
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 100 }); }}>
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/efectivo.png')} style={{ borderRadius: 8, width: 30 }}
+                                />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Efectivo</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25, backgroundColor: STheme.color.primary }}></SView>
+                        </SView>
+                    </SView>
+
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 200 }); }}>
+
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/tapeke.png')} style={{
+                                    borderRadius: 8,
+                                    width: 30
+                                }} />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Billetera Tapeke</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                        </SView>
+
+                    </SView>
+
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 300 }); }}>
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/Ptarjeta.png')} style={{
+                                    borderRadius: 8,
+                                    width: 30
+                                }} />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Tarjeta de Debito / Crédito</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                        </SView>
+                    </SView>
+
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 400 }); }}>
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/Pfassil.png')} style={{
+                                    borderRadius: 8,
+                                    width: 30
+                                }} />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Banco FASSIL</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                        </SView>
+                    </SView>
+
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 500 }); }}>
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/Ptransferencia.png')} style={{
+                                    borderRadius: 8,
+                                    width: 30
+                                }} />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Transferencia QR</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                        </SView>
+                    </SView>
+
+                    <SHr height={15} />
+
+                    <SView col={"xs-12"} row center onPress={() => { this.setState({ pagado: 600 }); }}>
+                        <SView col={"xs-2"} >
+                            <SView center style={{
+                                width: 40,
+                                height: 40, borderRadius: 8,
+                                borderWidth: 1, borderColor: STheme.color.lightGray
+                            }}>
+                                <SImage src={require('../../../../../Assets/img/Ptigo.png')} style={{
+                                    borderRadius: 8,
+                                    width: 30
+                                }} />
+                            </SView>
+                        </SView>
+                        <SView col={"xs-8"} >
+                            <SText fontSize={16} font={"Roboto"} style={{ fontWeight: "bold" }}>Tigo Money</SText>
+                        </SView>
+                        <SView col={"xs-2"} center>
+                            <SView width={18} height={18} style={{ borderWidth: 1, borderColor: STheme.color.lightGray, borderRadius: 25 }}></SView>
+                        </SView>
+                    </SView>
+
+                </SView>
+            </SView>
+            <SHr height={20} />
+            {/* </SView> */}
+        </>
+    }
+
+
+
+
     render() {
         return (
-            this.item()
+            this.getTipoPago()
         );
     }
 }
 
 const initStates = (state) => {
-	return { state }
+    return { state }
 };
 export default connect(initStates)(TipoPago);
--- app/src/Services/Tapeke/Components/billetera/Pages/CargarCredito.js
+++ app/src/Services/Tapeke/Components/billetera/Pages/CargarCredito.js
@@ -1,61 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { SHr, SNavigation, SPage, SText, SView, STheme, SLoad, SButtom, SIcon, SWebView } from 'servisofts-component';
+import { SHr, SNavigation, SPage, SText, SView, STheme, SLoad, SButtom, SIcon, SWebView, SImage, SInput } from 'servisofts-component';
 import { WebView } from 'react-native';
 import PButtom from '../../../../../Components/PButtom';
+import TipoPago from '../Components/TipoPago';
 
-
+// backgroundColor={this.state.envio != 0 ? STheme.color.primary : "transparent"}
 class CargarCredito extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            cantidad: 100,
         };
     }
 
     getHeaderCredito() {
-        return <><SHr height={10} />
-            <SView col={"xs-12"} height={201} center>
-                <SText fontSize={24} color={'black'} font={"Roboto"} bold>¡Recarga creditos a tu billetera!</SText>
-                <SText fontSize={16} color={'#979797'} font={"Roboto"} >Compra creditos para facilitar la compra de tu packs.</SText>
+        return <>
+            <SHr height={30} />
+            <SView col={"xs-11"} border={'transparent'} center>
+                <SView width={300} border={'transparent'}>
+                    <SText fontSize={24} color={'black'} font={"Roboto"} bold center>¡Recarga creditos a tu billetera!</SText>
+                </SView>
+                <SHr height={25} />
+                <SView width={350} border={'transparent'}>
+                    <SText fontSize={16} color={'#979797'} font={"Roboto"} center>Compra creditos para facilitar la compra de tu packs.</SText>
+                </SView>
+
+
+                <SHr height={25} />
+
+                {/* <SInput type={'money'} defaultValue={1} /> */}
+
+                <SText fontSize={70} color={'#979797'} font={"Roboto"} center>Bs {this.state.cantidad} </SText>
+                <SHr height={20} />
+
             </SView>
         </>
     }
 
-    getBodyCredito() {
-        return <><SHr height={10} />
-            <SView col={"xs-12"} center >
-                <SText font={"Roboto"} color={STheme.color.primary} fontSize={18} style={{ paddingLeft: 10 }} >Tipo de pago</SText>
+
+
+
+    getBoton() {
+        return <>
+            <SView col={"xs-11"} row center  >
+                <SHr height={30} />
+                <PButtom fontSize={20} onPress={() => { alert('alvaro'); }}>CARGAR CRÉDITO</PButtom>
+                <SHr height={30} />
             </SView>
         </>
     }
 
     render() {
-        // var data = Parent.Actions.getAll(this.props);
-        // var usuarios = usuario.Actions.getAll(this.props);
-        // if (!data) return <SLoad />;
-        // if (!usuarios) return <SLoad />;
         return (
             <>
-                <SPage title={'Billetera'}>
+                <SPage title={'Cargar crédito'}>
                     <SView col={"xs-12"} row center >
-
-                        <SView col={"xs-11 sm-10 lg-8 xl-3.5"} row center border={'red'}>
-                            {this.getHeaderCredito()}
-                            <SHr height={18} color={STheme.color.card} />
-                        </SView>
-
-                        <SView col={"xs-11"} row center border={'red'}>
-                            {this.getBodyCredito()}
-                            <SHr height={18} color={STheme.color.card} />
-                        </SView>
-
-                        <SView col={"xs-11"} row center border={'red'}>
-                            <SHr height={10} />
-                            <PButtom fontSize={20} onPress={() => { alert('alvaro'); }}>CARGAR CRÉDITO</PButtom>
-                            <SHr height={10} />
-                        </SView>
-
-
+                        {this.getHeaderCredito()}
+                        <TipoPago />
+                        {this.getBoton()}
                     </SView>
                 </SPage>
             </>
