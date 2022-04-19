da5ca0917a7da2defa04d5bd3a9e5b00e26dfda5
Licethd <liceth.dfe@gmail.com>
2022-04-19 18:35:53 -0400

    okok

---
+++ app_restaurante/src/Assets/svg/pedido/index.ts
@@ -0,0 +1,11 @@
+import Carga, { ReactComponent as CargaW } from './carga.svg';
+import Aspa, { ReactComponent as AspaW } from './aspa.svg';
+
+const Assets = {
+
+	"Carga": { Native: Carga, Web: CargaW },
+	"Aspa": { Native: Aspa, Web: AspaW },
+
+}
+
+export default Assets;
