5aa44dc0c7319bd8acf44767bb9443486eac7c43
Alvaro Siles <alvarosiles1@gmail.com>
2022-05-09 19:21:04 -0400

    pana
    pana

--- app/src/Pages/Inicio.js
+++ app/src/Pages/Inicio.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { SHr, SIcon, SImage, SLoad, SNavigation, SPage, SScrollView2, SText, STheme, SView, SPopup, SForm, SButtom, SList, SSection, SDate } from "servisofts-component";
+import { SHr, SIcon, SImage, SLoad, SNavigation, SPage, SScrollView2, SText, STheme, SView, SPopup, SForm, SButtom, SList, SSection, SDate, SGradient } from "servisofts-component";
 import BarraSuperiorTapeke from "../Components/BarraSuperiorTapeke";
 import Direccion from "../Components/BarraSuperiorTapeke/Direccion";
 import PBarraFooter from "../Components/PBarraFooter";
@@ -12,6 +12,7 @@ import SSocket from 'servisofts-socket'
 import restaurante from "../Services/Tapeke/Components/restaurante";
 import favorito from "../Services/Tapeke/Components/favorito";
 import Item2 from "../Services/Tapeke/Components/restaurante/Components/Item2";
+import pedido from "../Services/Tapeke/Components/pedido";
 
 class Inicio extends Component {
   constructor(props) {
@@ -72,6 +73,76 @@ class Inicio extends Component {
       }} />
   }
 
+  pedidoencurso() {
+
+    var data = restaurante.Actions.getAllFilter({}, this.props);
+    var favUsuario = favorito.Actions.getByKeyUsuario(this.props.state.usuarioReducer.usuarioLog.key, this.props)
+    if (!data) return <SLoad />;
+    if (!favUsuario) return <SLoad />;
+    var arr = Object.values(data).filter((itm) => favUsuario.find((elm) => elm.key_restaurante == itm.key))
+    return <SList
+      data={arr}
+      space={16}
+      center
+      horizontal={true}
+      render={(obj, key) => {
+        return <SView width={320}     >
+          <Item2 data={obj} ></Item2>
+        </SView>
+      }} />
+  }
+
+  pedidoencurso1() {
+		// var data = pedido.Actions.getAll(this.props);
+		var data = pedido.Actions.getDetalle(this.props);
+		if (!data) return <SLoad />
+		const key_usuario = this.props.state.usuarioReducer.usuarioLog.key;
+		return <SList
+			data={data}
+			space={16}
+			filter={(item) => item.estado == '1' && item.key_usuario == key_usuario}
+			render={(obj, key) => {
+    console.log("romeo ",obj);
+    // restaurante.nombre
+
+				return <SView col={"xs-12 "} height={90} row border={STheme.color.card} style={{ borderRadius: 8, }} onPress={() => {
+					if (obj.state == "pagado") {
+						SNavigation.navigate("pedido/confirmacion", { key_pedido: obj.key });
+					}
+				}} >
+					<SView col={"xs-2"} center   >
+						<SView height={40} width={40} style={{ backgroundColor: '#E9EAEE', borderRadius: 50, }} center   >
+							<SImage src={`${SSocket.api.root}restaurante/${"key_restaurante"}`} style={{ borderRadius: 8, resizeMode: 'cover' }} />
+						</SView>
+					</SView>
+					<SView col={"xs-10"} row center  >
+						<SView col={"xs-10"} height={40} style={{ justifyContent: 'center', }}  >
+							{/* <SText fontSize={15} font={"Roboto"} color={STheme.color.text} >{obj['descripcion']}</SText> */}
+							{/* <SText fontSize={16} font={"Roboto"} color={STheme.color.text} > {obj['descripcion']} </SText> */}
+							<SHr height={5} />
+							{/* <SText fontSize={12} font={"Roboto"} color={STheme.color.gray} >{obj['direccion']}</SText> */}
+							{/* <SText fontSize={12} font={"Roboto"} color={STheme.color.gray} >{new SDate(obj['fecha_on']).toString("dd-MM-yyyy hh:mm")} - {obj['state']} </SText> */}
+							<SText fontSize={12} font={"Roboto"} color={STheme.color.gray} >{obj['state']?"Tu pedido esta en camino":"no lo se"} </SText>
+						</SView>
+						<SView col={"xs-2"} height={40} style={{ alignContent: 'center', }}>
+							<SView height={36} width={36} center   >
+								{/* <SText fontSize={18} font={"Roboto"} color={STheme.color.gray} >x{obj['cantidad']}</SText> */}
+								<SText fontSize={18} font={"Roboto"} color={STheme.color.gray} >x Cancelar</SText>
+							</SView>
+						</SView>
+					</SView>
+					<SView col={"xs-12"} row   >
+
+						<SView col={"xs-6"} center onPress={() => { SNavigation.navigate('comoteparecio') }}   >
+							<SText width={120} height={20} style={{ backgroundColor: '#EEEEEE', borderRadius: 4, fontSize: 14, alignItems: 'center', }} center >Ver los detalles > </SText>
+						</SView>
+			 
+					</SView>
+				</SView>
+			}} />
+	}
+
+ 
 
   categoria(title) {
     return (
@@ -122,6 +193,16 @@ class Inicio extends Component {
         </BarraSuperiorTapeke>
         <SPage title={"as"} hidden center  >
           <SView col={"xs-12 md-12 lg-10 xl-8"} center height>
+            {this.categoria("Pedido en curso")}
+            {/* TODO ES PARA PROBAR, DE COMO SE VE */}
+            <SView col={"xs-12"} height={195} border={"transparent"} >
+              <SScrollView2>
+                {this.pedidoencurso()}
+              </SScrollView2>
+            </SView>
+
+           
+           
             {this.categoria("Recomendado Para Ti")}
             {/* TODO ES PARA PROBAR, DE COMO SE VE */}
             <SView col={"xs-12"} height={195} border={"transparent"} >
@@ -130,6 +211,9 @@ class Inicio extends Component {
               </SScrollView2>
             </SView>
             {this.categoria("Cerca")}
+
+
+            
             {/* TODO ES PARA PROBAR, DE COMO SE VE */}
             <SView col={"xs-12"} height={195} border={"transparent"} >
               <SScrollView2>
