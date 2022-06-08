e4aa91d2e1daa543184c5dd33368f5d792bdf13f
Licethd <liceth.dfe@gmail.com>
2022-06-07 21:32:07 -0400

    HH

--- app_restaurante/src/Services/Tapeke/Components/usuario_restaurante/Pages/lista002.js
+++ app_restaurante/src/Services/Tapeke/Components/usuario_restaurante/Pages/lista002.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { SHr, SImage, SList, SLoad, SNavigation, SPage, SPopup, SText, STheme, SThread, SView } from 'servisofts-component';
+import { SHr, SImage, SList, SLoad, SNavigation, SPage, SPopup, SText, STheme, SThread, SView, SIcon } from 'servisofts-component';
 import SSocket from 'servisofts-socket';
 import Parent from '..';
 import usuario from '../../../../Usuario/Components/usuario';
 import restaurante from '../../restaurante';
+import PButtom from '../../../../../Components/PButtom';
+
 class registro extends Component {
     constructor(props) {
         super(props);
@@ -16,6 +18,7 @@ class registro extends Component {
         if (!data) return <SLoad />
         var dataRestaurante = restaurante.Actions.getAll(this.props);
         if (!dataRestaurante) return <SLoad />
+        //alert(Object.keys(dataRestaurante).find(key => dataRestaurante[key].key_usuario === this.props.state.usuarioReducer.usuarioLog.key));
 
         new SThread(200, "esperaalgo", false).start(() => {
             var arr = Object.values(data).filter(x => x.key_usuario == this.props.state.usuarioReducer.usuarioLog.key && x.estado == 1);
@@ -29,41 +32,51 @@ class registro extends Component {
                 <SText fontSize={20} font={"Roboto"} color={STheme.color.darkGray} style={{ fontWeight: "bold", }}>MIS RESTAURANTES</SText>
             </SView>
             <SHr height={20} />
-
-            <SList data={dataRestaurante} center space={16}
-                filter={obj => {
-                    if (obj.estado != 1) return false;
-                    return data.find(o => o.key_restaurante == obj.key)
-                }}
-                render={obj => {
-                    return <SView col={"xs-12"} backgroundColor={STheme.color.card} row
-                        style={{
-                            borderWidth: 2, borderColor: STheme.color.lightGray,
-                            borderRadius: 8, overflow: 'hidden'
-                        }} center onPress={() => {
-                            SPopup.confirm({
-                                title: "¿Está seguro que desea ir a este restaurante?", message: `${obj.nombre}`,
-                                onPress: () => {
-                                    SNavigation.navigate("inicio", { key_restaurante: `${obj.key}` });
-                                }
-                            })
-                        }}>
-                        <SView col={"xs-3"} center row>
-                            <SHr height={10} />
-                            <SView width={70} height={70} style={{ borderRadius: 40, overflow: 'hidden', backgroundColor: '#eee', }}>
-                                <SImage src={SSocket.api.root + "restaurante/" + obj.key} style={{ resizeMode: "cover", }} />
+            {(Object.keys(dataRestaurante).find(key => dataRestaurante[key].key_usuario === this.props.state.usuarioReducer.usuarioLog.key)) ? //validando si el usuario tiene un restaurante asignado
+                <SList data={dataRestaurante} center space={16}
+                    filter={obj => {
+                        if (obj.estado != 1) return false;
+                        return data.find(o => o.key_restaurante == obj.key)
+                    }}
+                    render={obj => {
+                        return <SView col={"xs-12"} backgroundColor={STheme.color.card} row
+                            style={{
+                                borderWidth: 2, borderColor: STheme.color.lightGray,
+                                borderRadius: 8, overflow: 'hidden'
+                            }} center onPress={() => {
+                                SPopup.confirm({
+                                    title: "¿Está seguro que desea ir a este restaurante?", message: `${obj.nombre}`,
+                                    onPress: () => {
+                                        SNavigation.navigate("inicio", { key_restaurante: `${obj.key}` });
+                                    }
+                                })
+                            }}>
+                            <SView col={"xs-3"} center row>
+                                <SHr height={10} />
+                                <SView width={70} height={70} style={{ borderRadius: 40, overflow: 'hidden', backgroundColor: '#eee', }}>
+                                    <SImage src={SSocket.api.root + "restaurante/" + obj.key} style={{ resizeMode: "cover", }} />
+                                </SView>
+                                <SHr height={10} />
                             </SView>
-                            <SHr height={10} />
+                            <SView flex col={"xs-9"}>
+                                <SHr height={10} />
+                                <SText font={"Roboto"} fontSize={16} bold color={STheme.color.primary}>{`${obj.nombre}  `}</SText>
+                                <SText font={"Roboto"} fontSize={13}>{`${obj.descripcion}`}</SText>
+                                <SText font={"Roboto"} fontSize={12} style={{ fontStyle: "italic" }}>{`${obj.direccion}`}</SText>
+                                <SHr height={10} />
+                            </SView>
                         </SView>
-                        <SView flex col={"xs-9"}>
-                            <SHr height={10} />
-                            <SText font={"Roboto"} fontSize={16} bold color={STheme.color.primary}>{`${obj.nombre}  `}</SText>
-                            <SText font={"Roboto"} fontSize={13}>{`${obj.descripcion}`}</SText>
-                            <SText font={"Roboto"} fontSize={12} style={{ fontStyle: "italic" }}>{`${obj.direccion}`}</SText>
-                            <SHr height={10} />
-                        </SView>
-                    </SView>
-                }} />
+                    }} /> :
+                <SView col={"xs-12"} center>
+                    <SHr height={20} />
+                    <SText fontSize={15} font={"Roboto"} style={{ fontWeight: "bold" }}>No tiene restaurante asignado</SText>
+                    <SHr height={20} />
+                    <PButtom fontSize={20} onPress={() => {
+                        this.props.dispatch({ type: "USUARIO_LOGOUT" }); SNavigation.replace("login"); this.fadeOut();
+                    }}> <SIcon fill="#FFFFFF" name={"Exit"} width={28} height={27} /> SALIR</PButtom>
+                </SView>
+            }
+
         </>
     }
 
